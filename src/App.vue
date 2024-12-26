7875f7fd41b3c3b3eec3ca132338ee07b42d4b70
Hugues eyong <hugues777@outlook.com>
2024-12-26 12:43:12 +0100

    Ajout Questions

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,40 @@
 <template>
-  <h2>Bonjour</h2>
+  <div v-if="state === 'error'">
+    <p>Impossible de charger le quiz</p>
+  </div>
+  <div :aria-busy="state === 'loading'">
+    <Quiz :quiz="quiz"  v-if="quiz" />
+  </div>
 </template>
 
 <script setup>
-//charger Json
+import { onMounted, ref } from 'vue';
+import Quiz from './components/Quiz.vue';
 
+const quiz = ref(null)
+const state = ref('loading')
+//charger Json
+onMounted(() =>{
+  fetch('/quiz.json')
+    .then(r =>{
+      if(r.ok){
+        return r.json()
+      }
+      throw new Error('Impossible de recuperer le json')
+    })
+    .then(data =>{
+      quiz.value = data
+      state.value = 'idle'
+    })
+    .catch(e => {
+      state.value = 'error'
+    })
+})
 
 </script>
+
+<style>
+  .container{
+    margin-top: 2rem;
+  }
+</style>
---
+++ src/components/Question.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="question">
+        <h3>{{ question.question }}</h3>
+        <ul>
+            <li v-for="(choice, index) in question.choices" :key="choice">
+                <label :for="`answer${index}`">
+                    <input :id="`answer${index}`" type="radio" name="answer" :value="choice" v-model="answer">
+                    {{ choice }}
+                </label>
+            </li>
+        </ul>
+        <button :disabled="!hasAnswer" @click="emits('answer', answer)">Question suivante</button>
+    </div>
+</template>
+
+<script setup>
+    import { computed, ref } from 'vue';
+
+    const props = defineProps({
+        question: Object
+    })
+
+    //les evenements
+    const emits = defineEmits(['answer'])
+    const answer = ref(null)
+    const hasAnswer = computed(() => answer.value !== null) 
+</script>
+
+<style>
+    .question{
+        padding-top: 1rem;
+    }
+    .question button{
+        margin-left: auto;
+        display: block;
+    }
+
+</style>
